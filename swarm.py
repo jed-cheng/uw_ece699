7acfc5d5b8b3c4fce00e6a16fd6d4d21aee0497c
jedcheng <jedcheng@outlook.com>
2024-10-20 23:33:37 -0400

    able to coverage control

--- swarm.py
+++ swarm.py
@@ -79,6 +79,7 @@ class Swarm:
     return vor_cell
 
 
+
   def converage_control(self):
     robot_locations = self.get_robot_locations()
     density_function = self.density_functions[0]
@@ -119,7 +120,7 @@ class Swarm:
 
 
     self.__plot_environment()
-    self.__plot_voronoi_cell()
+    self.__plot_voronoi()
 
     self.axes.autoscale()
     self.axes.axis('equal')
@@ -131,7 +132,7 @@ class Swarm:
     for robot in self.robots:
       robot.update_plot()
     
-    self.__plot_voronoi_cell()
+    self.__plot_voronoi()
 
     self.figure.canvas.draw_idle()
     self.figure.canvas.flush_events()
@@ -139,6 +140,27 @@ class Swarm:
   def __plot_environment(self):
     p_env = patches.Polygon(self.environment, fill=False)
     self.axes.add_patch(p_env)
+
+  def __plot_voronoi(self):
+    vor_centroid, vor_cell, area = self.converage_control()
+    
+    for p_vor_centroid in self.p_vor_centroid:
+      p_vor_centroid.remove()
+    for p_vor_cell in self.p_vor_cell:
+      p_vor_cell.remove()
+
+    self.p_vor_centroid = []
+    self.p_vor_cell = []
+
+    for centroid in vor_centroid:
+      p = patches.Circle(centroid, radius=0.1, fill=True)
+      self.p_vor_centroid.append(p)
+      self.axes.add_patch(p)
+
+    for cell in vor_cell:
+      p = patches.Polygon(cell, fill=False, closed=True)
+      self.p_vor_cell.append(p)
+      self.axes.add_patch(p)
 
   # def __plot_density(self, density_functions=None):
   #   # plot  the current density functions
@@ -156,79 +178,6 @@ class Swarm:
   #       density_function.X, 
   #       density_function.Y, 
   #       density_function.Z, levels=10, colors=density_function.color))
-
-  def __plot_voronoi_cell(self):
-    vor_cell = self.get_voronoi_cell()
-    for p_vor_cell in self.p_vor_cell:
-      p_vor_cell.remove()
-
-    self.p_vor_cell = []
-
-    for cell in vor_cell:
-      p = patches.Polygon(cell, fill=False, closed=True)
-      self.p_vor_cell.append(p)
-      self.axes.add_patch(p)
-
-    pass
-
-
-  # def plot_voronoi(self, **kw):
-  #   ax = self.axes
-  #   points = np.array([robot.robot_pose[:2] for robot in self.robots])
-  #   vor = Voronoi(points)
-
-  #   if self.fininite_segments_line is not None:
-  #     self.fininite_segments_line.remove()
-  #   if self.infinite_segments_line is not None:
-  #     self.infinite_segments_line.remove()
-
-  #   from matplotlib.collections import LineCollection
-
-  #   if vor.points.shape[1] != 2:
-  #       raise ValueError("Voronoi diagram is not 2-D")
-
-  #   if kw.get('show_points', True):
-  #       point_size = kw.get('point_size', None)
-  #       ax.plot(vor.points[:, 0], vor.points[:, 1], '.', markersize=point_size)
-  #   if kw.get('show_vertices', True):
-  #       ax.plot(vor.vertices[:, 0], vor.vertices[:, 1], 'o')
-
-  #   line_colors = kw.get('line_colors', 'k')
-  #   line_width = kw.get('line_width', 1.0)
-  #   line_alpha = kw.get('line_alpha', 1.0)
-
-  #   center = vor.points.mean(axis=0)
-  #   ptp_bound = np.ptp(vor.points, axis=0)
-
-  #   finite_segments = []
-  #   infinite_segments = []
-  #   for pointidx, simplex in zip(vor.ridge_points, vor.ridge_vertices):
-  #       simplex = np.asarray(simplex)
-  #       if np.all(simplex >= 0):
-  #           finite_segments.append(vor.vertices[simplex])
-  #       else:
-  #           i = simplex[simplex >= 0][0]  # finite end Voronoi vertex
-
-  #           t = vor.points[pointidx[1]] - vor.points[pointidx[0]]  # tangent
-  #           t /= np.linalg.norm(t)
-  #           n = np.array([-t[1], t[0]])  # normal
-
-  #           midpoint = vor.points[pointidx].mean(axis=0)
-  #           direction = np.sign(np.dot(midpoint - center, n)) * n
-  #           if (vor.furthest_site):
-  #               direction = -direction
-  #           aspect_factor = abs(ptp_bound.max() / ptp_bound.min())
-  #           far_point = vor.vertices[i] + direction * ptp_bound.max() * aspect_factor
-
-  #           infinite_segments.append([vor.vertices[i], far_point])
-
-  #   finite_segments_line = LineCollection(finite_segments, colors=line_colors, lw=line_width, alpha=line_alpha, linestyle='solid')
-  #   self.fininite_segments_line = finite_segments_line
-  #   ax.add_collection(finite_segments_line)
-
-  #   infinite_segments_line = LineCollection(infinite_segments, colors=line_colors, lw=line_width, alpha=line_alpha, linestyle='dashed')
-  #   self.infinite_segments_line = infinite_segments_line
-  #   ax.add_collection(infinite_segments_line)
 
 
 
@@ -249,9 +198,15 @@ if __name__ == "__main__":
   robot_2 = Robot( robot_pose=[-5, -5, -1])
   robot_3 = Robot( robot_pose=[5, -5, 0.0])
   robot_4 = Robot( robot_pose=[-5, 5, 0.0])
-  robots = [robot_1, robot_2, robot_3]
+  robots = [robot_1, robot_2, robot_3, robot_4]
 
-  density_functions = []
+  density_functions = [
+    DensityFunction(
+      type='gaussian',
+      phi = lambda x, y: np.exp(-0.5 * ((x-5)**2 + (y-5)**2))/ (2 * np.pi),
+      color=None
+    )
+  ]
 
   env = np.array([
     [10, 10],
@@ -264,7 +219,10 @@ if __name__ == "__main__":
   swarm = Swarm(robots, env, density_functions)
 
 
-  for i in range(100):
-    robot_2.move_to_point(np.array([10, 10]))
+  for i in range(1000):
+    vor_centroid, vor_cell, area = swarm.converage_control()
+    for i, robot in enumerate(robots):
+      robot.move_to_point(vor_centroid[i])
+
     swarm.update_plot()
-    time.sleep(0.01)
+    time.sleep(0.001)
