efff17a4cdcb676785d5582d3a47fa83ae95ce4c
jedcheng <jedcheng@outlook.com>
2024-11-16 22:37:00 -0500

    add moving density

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from multiprocessing import Queue, Process
+from queue import Empty
 from music import get_audio_chords,  get_audio_tempo
-from pipeline import ColorPipeline, EmotionPipeline, LocationPipeline
+from pipeline import ColorPipeline, EmotionPipeline, LocationPipeline, cart2pol
 from simulator import Simulator
 from robot import Robot
 from swarm import Swarm
-import select
+import math
 
 import numpy as np
 
@@ -48,39 +49,47 @@ def proc_simulator(queue):
   swarm = Swarm(robots, env)
   sim = Simulator(swarm, env)
 
-  density_functions = []
+  colors = None
+  location = None
+  tempo = None
+
+  phi = 0
+  rho = 1
+
 
   sim.plot_environment(env)
   sim.plot_swarm(swarm)
   sim.plot()
 
-  prev_cursor_pos = None
 
   while True:
-    readable, _, _ = select.select([queue], [], [], 0.1)  # 0.1 is the timeout in seconds
-    if queue in readable:
-      message = queue.recv()
-      if message == 'exit':
-          queue.close()
-          break
-      elif message is not None:
-        print('simulator receive:', message)
+    try:
+      item = queue.get_nowait()
+      if item is None:
+        break
+      else:
+        i_colors, i_location, i_tempo = item
+        colors = i_colors
+        location = i_location
+        tempo = i_tempo
 
-        density_functions = [
-          DensityFunction(
-            type='gaussian',
-            color=color.value,
-            center= sim.cursor_pos if sim.cursor_pos is not None else location,
-            variance=[3, 3]
-          ) for color, location in message
-        ]
-        sim.plot_density_functions(density_functions)
-
-
+    except Empty:
+      pass
     
-    prev_cursor_pos = sim.cursor_pos
-    vor_robots, vor_prime = swarm.color_coverage_control(density_functions)
+    if location is None or colors is None:
+      continue
 
+    coord = [location[0] + np.cos(math.radians(phi)) * rho, location[1] + np.sin(math.radians(phi)) * rho]
+    density_functions = [
+      DensityFunction(
+        type='gaussian',
+        color=color.value,
+        center= coord,
+        variance=[2, 2]
+      ) for color in colors
+    ]
+
+    vor_robots, vor_prime = swarm.color_coverage_control(density_functions)
 
     for j in range(len(robots)):
       robot = robots[j]
@@ -89,7 +98,6 @@ def proc_simulator(queue):
       if vor_robot is None:
         continue
       
-      # vw = robot.coverage_control(vor_robot, L=1, delta=10)
       vw = robot.coverage_control(vor_robot, delta=10)
       
       color = robot.mix_color(vor_robot,
@@ -98,12 +106,21 @@ def proc_simulator(queue):
         swarm.yellow_density_functions
       )
 
+    phi = (phi + 5) % 360
 
-
-
-
+    sim.plot_density_functions(density_functions)
     sim.plot_swarm(swarm)
     sim.update_plot()
+
+
+
+
+
+
+
+
+
+
 
 
 def proc_pipeline(file_path, queue):
@@ -116,24 +133,27 @@ def proc_pipeline(file_path, queue):
 
   for i in range(min(len(chords), len(tempos))):
     chord = chords[i]
-    emotion = emotion_pipeline.predict_emotion(chord)
-
-    color = color_pipeline.predict_color(emotion)
+    tempo = tempos[i]
+    emotions = emotion_pipeline.predict_emotion(chord)
+    colors = color_pipeline.predict_colors(emotions)
     location = location_pipeline.predict_location(chord)
 
+
+
     # zip color and location
-    output = list(zip(color, location))
-    # print("pipeline send:", output)
-    # queue.send(output)
+    output = (colors, location, tempo)
+    queue.put(output)
     time.sleep(1)
+
+  queue.put(None)
 
 
 if __name__ == '__main__':
     queue = Queue()
     audio_file = 'c.mp3'
-    # sim_p = Process(target=proc_simulator, args=(queue,))
+    sim_p = Process(target=proc_simulator, args=(queue,))
     pipe_p = Process(target=proc_pipeline, args=(audio_file, queue))
-    # sim_p.start()
+    sim_p.start()
     pipe_p.start()
 
 
@@ -141,7 +161,7 @@ if __name__ == '__main__':
   # listen to music
   # send to pipeline
 
-    # sim_p.join()
+    sim_p.join()
     pipe_p.join()
     queue.close()
     print('done')
